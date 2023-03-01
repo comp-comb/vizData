0315c39bae44f307db4e31237d2e90c6280030ca
Khaled Mosharraf Mukut <32664955+kmmukut@users.noreply.github.com>
2023-03-01 17:21:27 -0600

    Update montage.sh

--- montage.sh
+++ montage.sh
@@ -20,7 +20,12 @@ montage Sun*Number*png \
     Wed*Number*png -font DejaVu-Serif Fri*Number*png -pointsize 120 \
     -label "$(labels 0)" Sun*Con*png \
     -label "$(labels 1)" Wed*Con*png \
-    -label "$(labels 2)" Fri*Con*png -tile x2 -geometry 1700x1150 montage.png
+    -label "$(labels 2)" Fri*Con*png -tile x2 -geometry 1600x1080 montage.png
+
+convert montage.png -bordercolor white -border 0x150 -gravity north padding.png
+rm montage.png
+convert padding.png -gravity north -bordercolor white -extent 5100x3000 montage.png
+rm padding.png
 
 echo "done"
 
